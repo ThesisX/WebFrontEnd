39e2b1740bff15fea731d9922e6e9eca963d9f44
sathapornma <sathapornma@rmuti.ac.th>
2021-07-02 22:48:53 +0700

    manage page and fix Signin

--- Project_FrontendNew/src/App.js
+++ Project_FrontendNew/src/App.js
@@ -1,20 +1,18 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// import { Divider } from "@material-ui/core";
-// import Tabbar from "./Components/Tabbar/tabbar";
-// import  AppBar  from "./Components/Appbar/appbar";
-// import Signup from "./Views/SignupPage/Signup";
+import React, { useState } from "react";
 import Signin from "./Views/SigninPage/Signin";
+import Hello from './Views/HelloPage/Hello';
 
 function App() {
+
+  const [UserSignin, setUserSignin] = useState(false)
+  
   return (
-   <div>
-     {/* <Tabbar/> */}
-     {/* <AppBar/> */}
-     {/* <Signup/> */}
-     <Signin/>
-   </div>
+    <div>
+      {UserSignin ?
+        <Hello />
+        : <Signin />
+      }
+    </div>
   )
 }
 
--- Project_FrontendNew/src/Views/SigninPage/Signin.js
+++ Project_FrontendNew/src/Views/SigninPage/Signin.js
@@ -1,14 +1,12 @@
-import React ,{useState, useEffect} from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import FormControl from '@material-ui/core/FormControl';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import Button from '@material-ui/core/Button';
-import Alert from '@material-ui/lab/Alert';
+// import Alert from '@material-ui/lab/Alert';
+
+
 import axios from 'axios'
 import qs from 'qs';
 
@@ -21,83 +19,89 @@ const useStyles = makeStyles((theme) => ({
   },
   button: {
     margin: '30px',
-    
+
   },
 }));
 
 const Singin = () => {
-  
-  const [Username, setUsername] = useState("")
-  const [Pwd, setPwd] = useState("")
-  
-  let base_url = "http://172.18.41.162:8000"
+
+  let [Username, setUsername] = useState("sathaphornma")
+  let [Password, setPassword] = useState("string")
+
+  let BASE_URL = "http://127.0.0.1:8000"
 
   const classes = useStyles();
 
-  const handleSubmit = (e) => {
+   
+  const handleSignin = async (e) => {
     e.preventDefault()
 
-    const form_data = {
-      username : Username,
-      password: Pwd
+    let form_data = {
+      username: Username,
+      password: Password,
     };
 
-  
-    const options = {
+    let options = {
       method: 'POST',
-      headers: { 'content-type': 'application/x-www-form-urlencoded' },
+      headers: {'content-type': 'application/x-www-form-urlencoded'},
       data: qs.stringify(form_data),
-      url: base_url+'/token',
+      url: BASE_URL + '/token',
     };
 
-    console.log(form_data)
-
-    axios(options)
+    await axios(options)
     .then(res => {
-        console.log(res)
-        // console.log(res.data)
-        
-    });
-
-    axios.get(base_url+'/users/info')
-    .then(res => {
-      console.log("user : " + res)
+      console.log(res)
       // console.log(res.data)
-      
     });
+
+    // await axios.get(BASE_URL + '/users/info')
+    //   .then(res => {
+    //     console.log("user : " + res)
+    //     // console.log(res.data)
+    // });
 
   }
-  
-    return (
-        
-        <form onSubmit={handleSubmit}>
-     <Alert severity="error">This is an error alert — check it out!</Alert>
+
+  return (
+
+    <form onSubmit={handleSignin}>
+      {/* <Alert severity="error">This is an error alert — check it out!</Alert> */}
       <div className={classes.margin}>
-        <Grid container spacing={2} alignItems="flex-end">
-          <Grid item>
-            <AccountCircle />
-          </Grid>
 
-          <Grid item >
-            <TextField  id="input-with-icon-grid" label="USER" onChange={(e)=>setUsername(e.target.value)} value={Username}/>
-          </Grid>
-        </Grid> <br></br>
+        {/* Username Input */}
         <Grid container spacing={2} alignItems="flex-end">
-          <Grid item>
-            <AccountCircle />
-          </Grid>
-
+          <Grid item><AccountCircle /></Grid>
           <Grid item >
-            <TextField id="input-with-icon-grid" label="PASSWORD"  onChange={(e)=>setPwd(e.target.value)} value={Pwd} />
+            <TextField
+              id="input-with-icon-grid"
+              label="USER"
+              onChange={(e) =>
+                setUsername(e.target.value)}
+              value={Username}
+            />
           </Grid>
         </Grid>
         
+        {/* Password Input */}
+        <Grid container spacing={2} alignItems="flex-end">
+          <Grid item><AccountCircle /></Grid>
+          <Grid item >
+            <TextField
+              id="input-with-icon-grid"
+              type="password"
+              label="PASSWORD"
+              onChange={(e) => setPassword(e.target.value)}
+              value={Password}
+            />
+          </Grid>
+        </Grid>
+
         <Button className={classes.button} type="Submit" variant="outlined" color="secondary">
-            Submit
+          Submit
         </Button>
-    </div>
+      </div>
     </form>
-    );
+  );
 }
 
 export default Singin;

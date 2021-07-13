dbb2169fa633ca2117c133210b1509ccce912dae
Bthorb <bejapon.ki@rmuti.ac.th>
2021-07-13 15:56:45 +0700

    editAppbar

--- Project_FrontendNew/src/App.js
+++ Project_FrontendNew/src/App.js
@@ -4,24 +4,27 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import useToken from './useToken';
 import Signin from "./Views/SigninPage/Signin";
 import Hello from './Views/HelloPage/Hello';
+import MyAppBar from './Components/Appbar/MyAppBar';
 
 const App = () => {
-  const { token, setToken } = useToken();
-  console.log("My Token : " + token)
+  // const { token, setToken } = useToken();
+  // console.log("My Token : " + token)
 
-  if (!token) {
-    return <Signin setToken={setToken} />
-  }
+  // if (!token) {
+  //   return <Signin setToken={setToken} />
+  // }
 
   return (
-    <BrowserRouter>
-      <h1>Application</h1>
-      <Switch>
-        <Route exact path="/">
-          <Hello />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <h1>Application</h1>
+    //   <Switch>
+    //     <Route exact path="/">
+    //       {/* <Hello /> */}
+    //      
+    //     </Route>
+    //   </Switch>
+    // </BrowserRouter>
+    <MyAppBar/>
   )
 }
 

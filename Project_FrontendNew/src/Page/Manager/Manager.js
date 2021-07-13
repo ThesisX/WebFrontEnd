56b9d002405d6593c453aab8794e4c033669dfa3
sathapornma <sathapornma@rmuti.ac.th>
2021-07-13 12:42:07 +0700

    Manage Page added image.

--- Project_FrontendNew/src/Page/Manager/Manager.js
+++ Project_FrontendNew/src/Page/Manager/Manager.js
@@ -11,122 +11,90 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import IconButton from '@material-ui/core/IconButton';
-
+import Container from '@material-ui/core/Container';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import myimage from '../../imagetest/180.jpg';
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    width: '1085px',
+    // width: '1085px',
     display: 'flex',
-    margin: '80px',
-    marginLeft: '620px' 
+    // marginLeft: '620px'
   },
   paper: {
     backgroundColor: '#ddd6f3',
     height: 300,
     width: 200,
-    margin: 50,
+    float:'left',
+    marginRight:50
   },
   control: {
     padding: theme.spacing(2),
   },
 }));
 
+
 const Manager = () => {
-    {
+
   const [spacing, setSpacing] = React.useState(2);
   const classes = useStyles();
   const theme = useTheme();
 
 
-  const handleChange = (event) => {
-    setSpacing(Number(event.target.value));
-  };
-
   return (
-
-    <Card className={classes.root}>
-    <div className={classes.details}>
-      <CardContent className={classes.content}>
-        <Typography component="h5" variant="h5">
-          Live From Space
-        </Typography>
-        <Typography variant="subtitle1" color="textSecondary">
-          Mac Miller
-        </Typography>
-      </CardContent>
-      <div className={classes.controls}>
-        <IconButton aria-label="previous">
-        </IconButton>
-      </div>
-    </div>
-    <CardMedia
-      className={classes.cover}
-      image= "./Page/Manager/1.jpg"
-      title="Live from space album cover"
-    />
-  </Card>
-
-
-    
-    // <Grid container className={classes.root} spacing={2} >
-      
-    //   <Grid item xs ={7}>
-    //     <Grid container justifyContent="center" spacing={spacing}>
-    //       {[0, 1 ].map((value) => (
-    //         <Grid key={value} item>
-    //           <Paper className={classes.paper}
-    //           />
-             
-    //         </Grid>
-    //       ))}
-    //     </Grid>
-    //     <Typography variant="body2" color="textSecondary" component="p">
-    //         {
-    //           "Why First Minister of Scotland Nicola Sturgeon thinks GDP is the wrong measure of a country's success:"
-    //         }
-    //       </Typography>
-    //   </Grid> <br/>
+    // <div>
+    //   <Card className={classes.root}>
+    //     <div className={classes.details}>
+    //       <CardContent className={classes.content}>
+    //         <Typography component="h5" variant="h5">
+    //           Live From Space
+    //         </Typography>
+    //         <Typography variant="subtitle1" color="textSecondary">
+    //           Mac Miller
+    //         </Typography>
+    //       </CardContent>
+    //       <div className={classes.controls}>
+    //         <IconButton aria-label="previous">
+    //         </IconButton>
+    //       </div>
+    //     </div>
+    //     <CardMedia
+    //       className={classes.cover}
+    //       image=""
+    //       title="Live from space album cover"
+    //     />
+    //   </Card>
+    // </div>
 
 
-    //   <Grid item xs ={7}>
-    //     <Grid container justifyContent="center" spacing={spacing}>
-    //       {[0, 1 ].map((value) => (
-    //         <Grid key={value} item>
-    //           <Paper className={classes.paper} />
-    //         </Grid>
-    //       ))}
-    //     </Grid>
-    //   </Grid>
+    <Container maxWidth="sm">
+      <Grid container className={classes.root} spacing={2}>
+        <Grid item xs={12}>
+          <Grid container justifyContent="center" spacing={spacing}>
+            <Grid  item>
+              <Paper className={classes.paper}>
+                <img src={myimage}/>
+              </Paper>
+              <Paper className={classes.paper} />
+            </Grid>
+          </Grid>
+        </Grid>
 
-    //  {/* spacing
-    //   <Grid item xs={12}>
-    //     <Paper className={classes.control}>
-    //       <Grid container>
-    //         <Grid item>
-    //           <FormLabel>spacing</FormLabel>
-    //           <RadioGroup
-    //             name="spacing"
-    //             aria-label="spacing"
-    //             value={spacing.toString()}
-    //             onChange={handleChange}
-    //             row
-    //           >
-    //             {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((value) => (
-    //               <FormControlLabel
-    //                 key={value}
-    //                 value={value.toString()}
-    //                 control={<Radio />}
-    //                 label={value.toString()}
-    //               />
-    //             ))}
-    //           </RadioGroup>
-    //         </Grid>
-    //       </Grid>
-    //     </Paper>
-    //   </Grid> */}
-    // </Grid>
+        
+        <Grid item xs={12}>
+          <Grid container justifyContent="center" spacing={spacing}>
+            <Grid  item>
+              <Paper className={classes.paper}/>
+              <Paper className={classes.paper} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
-}
 
-export default Manager ;
+
+export default Manager;

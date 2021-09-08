b51d50a8d972a1f9a418292118df01a62cd0457d
sathapornma <sathapornma@rmuti.ac.th>
2021-09-08 22:15:18 +0700

    manage BASE_URL

--- rmuti-extron/src/service.js
+++ rmuti-extron/src/service.js
@@ -1,3 +1,4 @@
-export const ROOT_URL = "http://20.194.53.85";
-export const BASE_URL = "http://20.194.53.85:8001";
-// export const BASE_URL = "http://127.0.0.1:8000";
+const PORT = "8000";
+export const ROOT_URL = "http://127.0.0.1";
+// export const ROOT_URL = "http://20.194.53.85";
+export const BASE_URL = `${ROOT_URL}:${PORT}`;
